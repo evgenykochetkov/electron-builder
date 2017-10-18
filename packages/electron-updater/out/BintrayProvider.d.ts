9cb7ee1f6b9db68ca138911557a766c575b3317f
Evgeny Kochetkov <evgeny.kochetkov@me.com>
2017-10-18 13:53:10 +0300

    a very brutal way to be able to install it with npm

--- packages/electron-updater/out/BintrayProvider.d.ts
+++
@@ -1,9 +0,0 @@
-import { BintrayOptions, HttpExecutor, VersionInfo } from "builder-util-runtime";
-import { FileInfo, Provider } from "./main";
-export declare class BintrayProvider extends Provider<VersionInfo> {
-    private client;
-    setRequestHeaders(value: any): void;
-    constructor(configuration: BintrayOptions, httpExecutor: HttpExecutor<any>);
-    getLatestVersion(): Promise<VersionInfo>;
-    getUpdateFile(versionInfo: VersionInfo): Promise<FileInfo>;
-}
